3964412dbb4aee8273dd9812e3a2b2c15e6c750c
Jon Smith <jonsmiths099@gmail.com>
2020-08-04 11:39:08 -0400

    Remove 9

--- Libraries/linux_steam_env/linux_steam_env.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-void setLinuxEnv()
-{
-    putenv("SteamAppId=602960");
-    putenv("SteamGameId=602960");
-}
--- Libraries/webm_mem_playback/opus/win32/version.h
+++
@@ -1,1 +0,0 @@
-#define PACKAGE_VERSION "0.9.5.0-162-gba9a9469d-dirty"
